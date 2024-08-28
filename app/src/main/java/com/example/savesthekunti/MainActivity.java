a428beec83d1c4f5612f5f7e75c95473da9266bd
RodyZack <wildanad466@gmail.com>
2024-08-28 14:09:15 +0700

    rodyzack 28

--- app/src/main/java/com/example/savesthekunti/MainActivity.java
+++ app/src/main/java/com/example/savesthekunti/MainActivity.java
@@ -13,6 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private PopupWindow popupWindow;
+    private PopupWindow exitPopupWindow;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
         // Inisialisasi setting button
         ImageButton settingsBtn = findViewById(R.id.setting_button);
+        ImageButton quitButton = findViewById(R.id.btn_quit);
+
         settingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showSettingsPopup(view);
             }
         });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showExitPopup(view);
+            }
+        });
+
 
 
     }
@@ -39,8 +52,57 @@ public class MainActivity extends AppCompatActivity {
         int popupWidth = getResources().getDimensionPixelSize(R.dimen.popup_width);
         int popupHeight = getResources().getDimensionPixelSize(R.dimen.popup_height);
 
+
         popupWindow = new PopupWindow(popupView, popupWidth, popupHeight, true);
         popupWindow.setAnimationStyle(R.style.PopupAnimation);
         popupWindow.showAtLocation(anchorView, Gravity.CENTER, 0, 0);
     }
+
+
+    //Method untuk mendisplay Exit PopUp
+    private void showExitPopup(View anchorView) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.exit_layout, null);
+
+        // Ambil dimensi dari resources
+        int popupWidth = getResources().getDimensionPixelSize(R.dimen.popup_width);
+        int popupHeight = getResources().getDimensionPixelSize(R.dimen.popup_height);
+
+
+        exitPopupWindow = new PopupWindow(popupView, popupWidth, popupHeight, true);
+        exitPopupWindow.setAnimationStyle(R.style.PopupAnimation);
+        exitPopupWindow.showAtLocation(anchorView, Gravity.CENTER, 0, 0);
+
+        //inisialisasi YES & NO button
+        ImageButton yesBtn = popupView.findViewById(R.id.imageYes);
+        ImageButton noBtn  = popupView.findViewById(R.id.imageExit);
+
+        int yesNoWidth = getResources().getDimensionPixelSize(R.dimen.yesno_width);
+        int yesNoHeight = getResources().getDimensionPixelSize(R.dimen.yesno_height);
+
+        yesBtn.getLayoutParams().width = yesNoWidth;
+        yesBtn.getLayoutParams().height = yesNoHeight;
+
+        noBtn.getLayoutParams().width = yesNoWidth;
+        noBtn.getLayoutParams().height = yesNoHeight;
+
+
+        yesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        noBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exitPopupWindow.dismiss();
+            }
+        });
+
+    }
+
+
+
 }
