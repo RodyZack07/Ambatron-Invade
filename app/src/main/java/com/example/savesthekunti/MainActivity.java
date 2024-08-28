7477bbdf576a87a4388b44b0a21468068add57c7
Zkuu <zakariasemarang33@gmail.com>
2024-08-28 10:53:39 +0700

    Zkuu 28

--- app/src/main/java/com/example/savesthekunti/MainActivity.java
+++ app/src/main/java/com/example/savesthekunti/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.savesthekunti;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Inisialisasi setting button
         ImageButton settingsBtn = findViewById(R.id.setting_button);
         settingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
                 showSettingsPopup(view);
             }
         });
+
+
     }
 
     private void showSettingsPopup(View anchorView) {
@@ -42,5 +43,4 @@ public class MainActivity extends AppCompatActivity {
         popupWindow.setAnimationStyle(R.style.PopupAnimation);
         popupWindow.showAtLocation(anchorView, Gravity.CENTER, 0, 0);
     }
-
 }
